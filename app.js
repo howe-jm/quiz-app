a2b3e0c7fcc032766d9316d182239f0e5cd56295
Jason Howe <trisynth@gmail.com>
2020-09-16 16:34:01 -0400

    Initial commit.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+/* eslint-disable no-console */
 /* global $ */
-/* global cuid */
 
 'use strict';
 
@@ -10,14 +10,29 @@ const STORE = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
-      answers: ['red', 'orange', 'pink', 'green'],
-      correctAnswer: 'green',
+      question: 'Q1',
+      answers: ['A1', 'A2', 'A3', 'A4'],
+      correctAnswer: 'A2',
     },
     {
-      question: 'What is the current year?',
-      answers: ['1970', '2015', '2019', '2005'],
-      correctAnswer: '2019',
+      question: 'Q2',
+      answers: ['A1', 'A2', 'A3', 'A4'],
+      correctAnswer: 'A4',
+    },
+    {
+      question: 'Q3',
+      answers: ['A1', 'A2', 'A3', 'A4'],
+      correctAnswer: 'A3',
+    },
+    {
+      question: 'Q4',
+      answers: ['A1', 'A2', 'A3', 'A4'],
+      correctAnswer: 'A2',
+    },
+    {
+      question: 'Q5',
+      answers: ['A1', 'A2', 'A3', 'A4'],
+      correctAnswer: 'A1',
     },
   ],
   quizStarted: false,
@@ -25,10 +40,119 @@ const STORE = {
   score: 0,
 };
 
-function render() {}
+/*
+// Create the question HTML string
+// Display the beginning HTML
+// Display the results HTML
+
+/* Present each question with:
+    - 4 potential answers
+    - A running count of correct questions
+    - Total questions
+    - A button to submit the answer
+*/
+
+// A function that chooses what to display based on where the user is (Beginning, questions, results)
+function render() {
+  // console.log(STORE.questions.length);
+  // console.log(STORE.questionNumber);
+  if (STORE.quizStarted === false) {
+    $('.js-main').html(quizStartPage());
+  } else {
+    if (STORE.questionNumber < STORE.questions.length) {
+      $('.js-main').html(QuizQuestionPage(STORE, STORE.questionNumber));
+    } else {
+      $('.js-main').html(QuizResultsPage());
+    }
+  }
+}
+
+function quizStartPage() {
+  // console.log('quizStartPage ran');
+  const QuizzStartPageHTML = quizStartPageString();
+  return QuizzStartPageHTML;
+}
+
+function quizStartPageStartButton() {
+  // console.log('quizStartPageStartButton ran');
+  $('.js-main').on('click', '.js-start-page-submit', (evt) => {
+    evt.preventDefault();
+    STORE.quizStarted = true;
+    render();
+  });
+}
+
+function quizStartPageString() {
+  // console.log('quizStartPageStringGenerator ran');
+  return `
+  <section class="quiz-container">
+  <form class="quiz-form">
+    <button class="js-start-page-submit">START</button>
+  </form>
+  </section>
+`;
+}
+
+function QuizQuestionPage(array, qNum) {
+  // console.log('QuizQuestionPage ran');
+  const question = array.questions[qNum];
+  // console.log(question);
+  const questionHTML = quizQuestionStringGenerator(question);
+  return questionHTML;
+  // const quizQuestionString = quizQuestionStringGenerator();
+  // return quizQuestionString;
+}
+
+function questionSubmitButton() {
+  // console.log('quizSubmitButton ran');
+  $('.js-main').on('click', '.js-question-page-submit', (evt) => {
+    evt.preventDefault();
+    if ($('form input[type=radio]:checked').val() === STORE.questions[STORE.questionNumber].correctAnswer) {
+      STORE.score++;
+    }
+    STORE.questionNumber++;
+    render();
+  });
+}
+
+function quizQuestionStringGenerator(quest) {
+  // console.log(STORE.questions[STORE.questionNumber].correctAnswer)
+  return `
+  <section class="quiz-container">
+  <p>Cheat Mode Answer: ${STORE.questions[STORE.questionNumber].correctAnswer}</p>
+  <form id="quiz-form"><h4>${quest.question}</h4>
+    <input type="radio" id="ans1" name="answers" value="${quest.answers[0]}">
+    <label for="ans1">${quest.answers[0]}</label><br>
+    <input type="radio" id="ans1" name="answers" value="${quest.answers[1]}">
+    <label for="ans1">${quest.answers[1]}</label><br>
+    <input type="radio" id="ans1" name="answers" value="${quest.answers[2]}">
+    <label for="ans1">${quest.answers[2]}</label><br>
+    <input type="radio" id="ans1" name="answers" value="${quest.answers[3]}">
+    <label for="ans1">${quest.answers[3]}</label><br>
+    <button class="js-question-page-submit">SUBMIT ANSWER</button>
+  </form>
+  <p>Current score is ${STORE.score} out of ${STORE.questions.length}</p>
+  </section>
+  `;
+}
+
+function QuizResultsPage() {
+  // console.log('QuizResultsPage ran');
+  return `
+  <section class="quiz-container">
+  <p>Results Page</p>
+  <p>Final score is ${STORE.score} out of ${STORE.questions.length}</p>
+  <form class="quiz-form">
+    <button class="js-results-page-submit">Restart</button>
+  </form>
+  </section>
+  `;
+}
 
 function main() {
   render();
+  quizStartPageStartButton();
+  questionSubmitButton();
 }
 
 $(main);
